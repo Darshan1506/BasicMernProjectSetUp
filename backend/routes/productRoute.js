dfb5991affc4146edae008292fd626436b16345e
Darshan <darshanrajpurohit47@gmail.com>
2022-06-06 15:33:05 +0530

    api

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productController");
+const { getAllProducts , createProducts, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts)
+router.route("/product/new").post(createProducts)
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)
 
 module.exports = router;
